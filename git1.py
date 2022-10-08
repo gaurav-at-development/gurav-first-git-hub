bb4e24bf5c83fa46a276d1a75ac1773dc7d3596b
gaurav <gauravshukla.c1999@gmail.com>
2022-10-08 14:45:25 +0530

    added two files 1-> text read csv 2 -> updated commit

--- git1.py
+++ git1.py
@@ -2,5 +2,14 @@ print("this is git commands")
 '''
 mkdir name of file
 cd nameof file
+ls 
+ls -a
+$ git status
+$ git add git1.py
+$ git status
+$ git commit -m"initial commot":- this sholud be 
+a meaing ful massage
+git log -  for getting logs
+clear 
 
 '''
