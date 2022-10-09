678618629e3467bbc99dc68163a8e3747c6d8f38
gaurav <gauravshukla.c1999@gmail.com>
2022-10-09 20:32:23 +0530

    new java files

---
+++ VSCFILE/saample.java
@@ -0,0 +1,13 @@
+class A{  
+    static public void main(String... args){  
+    System.out.println("hello java4");
+}
+
+    public A() {
+    }
+
+    @Override
+    public String toString() {
+        return "A []";
+    };
+}
